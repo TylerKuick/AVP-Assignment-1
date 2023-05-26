ef999d22eb58273a037dde32212cb1f2f176e98b
220452P <131850519+220452P@users.noreply.github.com>
2023-05-26 13:52:58 +0800

    Update Program.cs
    Add Heal Feature
    Complete Combat System

--- AVP Assignment/PokemonPocket/Program.cs
+++ AVP Assignment/PokemonPocket/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using Function;
@@ -48,8 +48,8 @@ namespace PokemonPocket
                 string option = Console.ReadLine();
                 if (option == "1") {
                     Console.WriteLine("Enter Pokemon's Name: ");
-                    string pokemon_name = Console.ReadLine().ToLower();
-                    if (functions.checkName(pokemon_name, db)){
+                    string pokemon_name = Console.ReadLine();
+                    if (functions.checkName(pokemon_name.ToLower(), db)){
                         Console.WriteLine("Enter Pokemon's HP: ");
                         int pokemon_hp = int.Parse(Console.ReadLine());
                         Console.WriteLine("Enter Pokemon's Exp: ");
@@ -112,6 +112,15 @@ namespace PokemonPocket
                         string pokemon_name = Console.ReadLine().ToLower();
                         if (functions.checkEvolve(pokemon_name ,db, pokemonMasters)) { // If given pokemon can evolve
                             functions.Evolve(pokemon_name, db, pokemonMasters); // Evolve function
+                            foreach (var i in db) {
+                                Console.WriteLine(@$"
+                        ----------------------------------
+                        Pokemon Name: {i.Name}
+                        Pokemon HP: {i.HP}
+                        Pokemon Exp: {i.Exp}
+                        ----------------------------------
+                                ");
+                            }
                         }
                         else {
                             Console.WriteLine($"{pokemon_name} cannot be evolved"); 
@@ -126,8 +135,8 @@ namespace PokemonPocket
                     List<Pokemon> battle_pokemon = new List<Pokemon>();
                     // Create pokemon to fight with random HP 
                     var battle_pikachu = new Pikachu("Pikachu", rdm.Next(10, 90), 0);
-                    var battle_eevee = new Pikachu("Eevee", rdm.Next(10, 90), 0);
-                    var battle_char = new Pikachu("Charmander", rdm.Next(10, 90), 0);
+                    var battle_eevee = new Eevee("Eevee", rdm.Next(10, 90), 0);
+                    var battle_char = new Charmander("Charmander", rdm.Next(10, 90), 0);
                     // Decrement respective pokemon count to compensate for initialisation 
                     battle_pikachu.decCount();
                     battle_eevee.decCount();
@@ -139,53 +148,95 @@ namespace PokemonPocket
 
                     Boolean battle = true;
                     while (battle) {
-                        // Prompt user for chosen pokemon to use in battle
-                        Console.WriteLine("Choose Pokemon to use in battle: ");
-                        var pokemon_name = Console.ReadLine().ToLower();
-                        if (functions.checkName(pokemon_name, db)) {
-                            // Declare a random pokemon to battle against
-                            int opp_ind = rdm.Next(battle_pokemon.Count);
-                            var opp = battle_pokemon[opp_ind];
-                            // Take the first (most hp) pokemon in db with the given name
-                            var pokemon = db.Where(p=>p.Name.ToLower() == pokemon_name).First();
-
-                            // Combat 
-                            while (pokemon.HP > 0 || opp.HP > 0) {
-                                // Opponent attacks first 
-                                Console.WriteLine($"{opp.Name} (Opp) used {opp.Skill}!");
-                                pokemon.calculateDamage(opp.Skill_Dmg); // Pokemon takes damage
-                                if (pokemon.HP == 0) {  // If pokemon fainted, player loses
-                                    Console.WriteLine($"{pokemon.Name} has fainted");
-                                    Console.WriteLine("You Lose!");
+                        // Find all pokemon that can battle 
+                        var pokemon_list = db.Where(p=>p.HP > 0).ToList();
+                        if (pokemon_list.Count == 0) {  // If there are no pokemon that can battle,
+                            Console.WriteLine("All your pokemon have fainted!");    // Alert player
+                            break;      // Go back to menu 
+                        }
+                        else {
+                            // Prompt user for chosen pokemon to use in battle
+                            Console.WriteLine("Choose Pokemon to use in battle: ");
+                            var pokemon_name = Console.ReadLine().ToLower();
+                            if (functions.checkName(pokemon_name, pokemon_list)) {
+                                // Declare a random pokemon to battle against
+                                int opp_ind = rdm.Next(battle_pokemon.Count);
+                                var opp = battle_pokemon[opp_ind];
+                                try {
+                                    // Take the first (most hp) pokemon in db with the given name
+                                    var pokemon = db.Where(p=>p.Name.ToLower() == pokemon_name && p.HP > 0).First();
+                                    // Combat 
+                                    while (pokemon.HP > 0 && opp.HP > 0) {
+                                    // Opponent attacks first 
+                                        Console.WriteLine($"{opp.Name} (Opp) used {opp.Skill}!");
+                                        pokemon.calculateDamage(opp.Skill_Dmg); // Pokemon takes damage
+                                        if (pokemon.HP <= 0) {  // If pokemon fainted, player loses
+                                            Console.WriteLine($"{pokemon.Name} has fainted");
+                                            Console.WriteLine("You Lose!");
+                                        }
+                                        else {
+                                            // Show player's pokemon HP left
+                                            Console.WriteLine($"{pokemon.Name} has {pokemon.HP} HP left!");
+                                            // Player's pokemon attacks opponent
+                                            Console.WriteLine($"{pokemon.Name} used {pokemon.Skill}!");
+                                            opp.calculateDamage(pokemon.Skill_Dmg); // Opponent takes damage
+                                            if (opp.HP <= 0) { // If opponent pokemon fainted, player wins
+                                                Console.WriteLine($"{opp.Name} (Opp) has fainted!");
+                                                Console.WriteLine("You Win!");
+                                            }
+                                            else {
+                                                // Show opponent HP left
+                                                Console.WriteLine($"{opp.Name} (Opp) has {opp.HP} HP left!");
+                                            }
+                                        }
+                                    }   
                                 }
-                                else {
-                                    // Show player's pokemon HP left
-                                    Console.WriteLine($"{pokemon.Name} has {pokemon.HP} left!");
-                                    // Player's pokemon attacks opponent
-                                    Console.WriteLine($"{pokemon.Name} used {pokemon.Skill}!");
-                                    opp.calculateDamage(pokemon.Skill_Dmg); // Opponent takes damage
-                                    if (opp.HP == 0) { // If opponent pokemon fainted, player wins
-                                        Console.WriteLine($"{opp.Name} has fainted!");
-                                        Console.WriteLine("You Win!")
-                                    }
-                                    else {
-                                        // Show opponent HP left
-                                        Console.WriteLine($"{opp.Name} has {op.HP} left!")
-                                    }
+                                catch (System.InvalidOperationException) {
+                                    Console.WriteLine($"{pokemon_name} has already fainted");
+                                }
+                                // Ask player if they want to continue
+                                Console.WriteLine("Do you want to continue (Y/N): ");
+                                string cont = Console.ReadLine().ToLower();
+                                // if player does not enter yes, break while loop 
+                                if (cont != "y") {
+                                    battle = false;
                                 }
                             }
-                            // Ask player if they want to continue
-                            Console.WriteLine("Do you want to continue (Y/N): ");
-                            string continue = Console.ReadLine().ToLower();
-                            // if player does not enter yes, break while loop 
-                            if (continue != "y") {
-                                battle = false;
+                            else if (!functions.checkName(pokemon_name, pokemon_list) && functions.checkName(pokemon_name, db)) {
+                                Console.WriteLine($"{pokemon_name} has already fainted!");
+                            }
+                            else {
+                                Console.WriteLine("Pokemon not found in database");
                             }
                         }
                     }
                 }
                 else if (option == "6") { // Heal Pokemon that has been damaged or fainted 
-                
+                    var heal_list = db.Where(p=> p.HP < p.Org_HP).ToList();     // If pokemon current HP less than original HP
+                    foreach (var i in heal_list) {  // Show list of pokemon that need healing
+                        Console.WriteLine(@$"
+                ----------------------------------
+                Pokemon Name: {i.Name}
+                Pokemon Org HP: {i.Org_HP}
+                Pokemon Curr HP: {i.HP}
+                ----------------------------------        
+                        ");
+                    }
+                    if (heal_list.Count > 0) {  // While there are pokemon that need healing 
+                        Console.WriteLine("Choose Pokemon to heal: ");
+                        var pokemon_name = Console.ReadLine().ToLower();
+                        if (functions.checkName(pokemon_name, heal_list)) { 
+                            var heal_pokemon = heal_list.Where(p=> p.Name.ToLower() == pokemon_name).First();
+                            functions.Heal(heal_pokemon);
+                            Console.WriteLine($"{pokemon_name} was successfully healed.");
+                        }
+                        else {
+                            Console.WriteLine($"Please enter a pokemon that needs healing.");
+                        }
+                    }
+                    else {
+                        Console.WriteLine("No pokemon need healing.");
+                    }
                 }
                 else {
                     Environment.Exit(0);
